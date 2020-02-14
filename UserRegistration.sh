8b7111aa2e201a7e08912c9ae07333900b529de9
kirtibhave <kirtibhave1000@gmail.com>
2020-02-14 19:00:19 +0530

    ADDED:Check validation of first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
 echo "**********Welcome to User Registration Problem**********"
+shopt -s extglob
+
+read -p  "enter firstname:" userFirstName
+
+firstNameRule="(^[A-Z]{1}[a-z]{2,}*)$"
+
+if [[ $userFirstName =~ $firstNameRule ]]
+then
+	echo "Firstname is correct"
+else
+	echo "Invalid Firstname...!!Enter proper Firstname"
+fi
